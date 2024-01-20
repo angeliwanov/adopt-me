8923014e9df002965fb71f83278374655c8199a7
Angel Ivanov <angel.iwanov@gmail.com>
2024-01-20 09:31:58 +0200

    testing

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 const Pet = ({ name, animal, images, location, id, breed }) => {
   let hero = "http:'//pets-images.dev-apis.com/pets/none.jpg";
 
-  if (images.length > 0) {
+  if (images && images.length > 0) {
     hero = images[0];
   }
 
   return (
     <Link to={`/details/${id}`} className="pet">
       <dir className="image-container">
-        <img src={hero} alt={name} />
+        <img data-testid="thumbnail" src={hero} alt={name} />
       </dir>
       <div className="info">
         <h1>{name}</h1>
